3eb784227b67df844d5d5769bb4230f7cb70713e
Andreas Rissling <a.rissling@steinberg.de>
2023-10-12 17:13:26 +0200

    feat: Add first content to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main>asd</main>;
+  return (
+    <main>
+      <h1>React playground</h1>
+      <p>This is my playground to test react and next features.</p>
+    </main>
+  );
 }
